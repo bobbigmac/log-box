bd697066b59ffc5f8e646f194f0ee9d2d44c60ea
bobbigmac <admin@bobbigmac.com>
2015-08-03 16:00:41 +0100

    Tweak router data, and add currentUser for waitOn

--- shared/prep/router.js
+++ shared/prep/router.js
@@ -4,10 +4,24 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
+var currentUser = {
+  ready: function() {
+    var user = Meteor.user();
+    return (user === null || typeof user !== "undefined");
+  }
+};
+
 Router.route('/', {
 	name: 'home',
 	waitOn: function () {
-		//return Meteor.subscribe('games');
+		//return [
+		//	currentUser,
+		//	Meteor.subscribe('games')
+		//];
+	},
+	data: function() {
+		//var links = Links.find();
+		//Session.set('page-title', links.count()+' links');
 	}
 });
 
