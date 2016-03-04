6f2aa4952b5939151a2b0853f8062137351f187b
Bob Davies <admin@bobbigmac.com>
2016-03-04 01:22:41 +0000

    Update '### time ago' reactively

--- components/Event.jsx
+++ components/Event.jsx
@@ -1,9 +1,30 @@
 Event = React.createClass({
+  mixins: [TimerMixin],
 	getInitialState() {
-	    return {
-	        detail: false  
-	    };
+		var event = this.props.event;
+		var now = new Date();
+		var date = new Date(event && event.created);
+		var seconds = Math.abs((now.getTime() - date.getTime()) / 1000);
+
+		var mom = new moment(date);
+		var waitSeconds = (seconds > 60*60 ? 60 : 10);
+
+    return {
+      detail: false,
+      moment: mom,
+      waitMs: waitSeconds * 1000,
+      fromNow: mom && mom.fromNow()
+    };
 	},
+  componentDidMount() {
+  	//Could use setTimeout and update the wait more specifically
+    this.setInterval(this.updateFromNow, this.state.waitSeconds);
+  },
+  updateFromNow() {
+  	const newNow = this.state.moment.fromNow();
+  	this.setState({ fromNow: newNow });
+  	//console.log('updated', newNow);
+  },
 	toggleDetail() {
 		this.setState({ detail: !this.state.detail });
 	},
@@ -19,7 +40,7 @@ Event = React.createClass({
 		return (
 			<div>
 				<h3 className={"event-"+item.level}>
-					{(item.created && <span className="pull-right">{new moment(new Date(item.created)).fromNow()}</span>)}
+					{(item.created && <span className="pull-right">{this.state.fromNow}</span>)}
 					<a className="event-title" onClick={this.toggleDetail}>
 						{(item.title || item.message || item.messageTemplate || item._id)}
 					</a>
